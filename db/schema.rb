4728cc13fcef18559c97c121f65be3a0af611f88
CLackrone <charisaanne@gmail.com>
2018-04-12 11:31:08 -0400

    migration to update :age_range type

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180412152105) do
+ActiveRecord::Schema.define(version: 20180412152455) do
 
   create_table "toys", force: :cascade do |t|
     t.string "name"
-    t.integer "age_range"
+    t.string "age_range"
     t.string "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
