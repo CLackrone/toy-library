a7f7168f46c428d70447bc7e6e67bb476c9d9754
CLackrone <charisaanne@gmail.com>
2018-04-13 12:05:45 -0400

    reseeded db with correct image_urls

--- client/src/components/Toys.js
+++ client/src/components/Toys.js
@@ -3,9 +3,9 @@ import React, { Component } from 'react'
 export default class Toys extends Component {
   render() {
     const toys = [
-      {"id":9,"name":"Barbie House","age_range":"5-10","image_url":"https://imgur.com/FMwMstR","created_at":"2018-04-12T15:51:22.914Z","updated_at":"2018-04-12T15:51:22.914Z"},
-      {"id":5,"name":"BeatBelle","age_range":"1-4","image_url":"https://imgur.com/RLjP9hE","created_at":"2018-04-12T15:51:22.905Z","updated_at":"2018-04-12T15:51:22.905Z"},
-      {"id":10,"name":"Bow and Arrows","age_range":"5-10","image_url":"https://imgur.com/hwDhmMu","created_at":"2018-04-12T15:51:22.917Z","updated_at":"2018-04-12T15:51:22.917Z"}
+      {"id":9,"name":"Barbie House","age_range":"5-10","image_url":"https://imgur.com/FMwMstR.png","created_at":"2018-04-12T15:51:22.914Z","updated_at":"2018-04-12T15:51:22.914Z"},
+      {"id":5,"name":"BeatBelle","age_range":"1-4","image_url":"https://imgur.com/RLjP9hE.png","created_at":"2018-04-12T15:51:22.905Z","updated_at":"2018-04-12T15:51:22.905Z"},
+      {"id":10,"name":"Bow and Arrows","age_range":"5-10","image_url":"https://imgur.com/hwDhmMu.png","created_at":"2018-04-12T15:51:22.917Z","updated_at":"2018-04-12T15:51:22.917Z"}
     ]
 
     const renderToys = toys.map(toy =>
