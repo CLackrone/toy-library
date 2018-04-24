b9e5b267402f7b8da46175a227fb88c48e427195
CLackrone <charisaanne@gmail.com>
2018-04-24 10:26:02 -0400

    added default image to toyCard

--- client/src/components/ToyCard.js
+++ client/src/components/ToyCard.js
@@ -1,12 +1,22 @@
 import React from 'react'
 
 const ToyCard = ({ toy }) => 
+
   <div key={toy.id} className='container text-center'>
     <p>Toy Name: {toy.name}</p>
     <p>Age Range: {toy.age_range}</p>
     <p>Description: {toy.description} </p>
-    <img src={`${toy.image_url}`} style={{height: '220px', width: '221px', margin: 'auto'}} alt={toy.name} />
+    {toy.image_url? (
+      <img src={`${toy.image_url}`} style={{height: '220px', width: '221px', margin: 'auto'}} alt={toy.name} />
+      ) : (
+        <img src='https://x.kinja-static.com/assets/images/logos/placeholders/default.png'
+          style={{height: '220px', width: '221px', margin: 'auto'}} alt={toy.name}
+          />
+      )
+  }
   </div>
+
   
 export default ToyCard
+
 
