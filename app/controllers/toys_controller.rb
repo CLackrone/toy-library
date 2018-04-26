5c9af046ec35779cf4d22e168325809eaa4c97ca
CLackrone <charisaanne@gmail.com>
2018-04-26 12:27:01 -0400

    toyDelete function deleting toy, removing from state

--- app/controllers/toys_controller.rb
+++ app/controllers/toys_controller.rb
@@ -1,5 +1,6 @@
 class ToysController < ApplicationController
-  
+  before_action :set_toy, only: [:destroy]
+
   def index
     @toys = Toy.all.order('name ASC')
     render json: @toys
@@ -14,10 +15,23 @@ class ToysController < ApplicationController
     end
   end
 
+  def destroy
+    if @toy.destroy
+      render status: 204
+    else
+      render json: { message: "Cannot delete toy"}, status: 400
+    end
+  end
+
   private
 
   def toy_params
     params.permit(:name, :age_range, :image_url, :description, :borrowed)
   end
 
+  def set_toy
+    @toy = Toy.find_by(id: params[:id])
+  end
+
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
-  scope '/api' do
-    get :toys, to: 'toys#index'
-    post :toys, to: 'toys#create'
+
+  scope '/api' do 
+    resources :toys, except: [:new, :edit, :show]
   end
+  
 end
+
+
+#scope '/api' do
+    #get :toys, to: 'toys#index'
+    #post :toys, to: 'toys#create'
+  #end
