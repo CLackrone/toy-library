004ce059a0f08b39faa891d07cb251bc369a3a6e
CLackrone <charisaanne@gmail.com>
2018-04-27 12:13:47 -0400

    defined UPDATE_TOY reducer

--- client/src/reducers/toys_reducer.js
+++ client/src/reducers/toys_reducer.js
@@ -4,11 +4,13 @@ export default (state = [], action) => {
       return action.toys
     case 'ADD_TOY':
       return [...state, action.toy]
+    case 'UPDATE_TOY':
+      //filter out all toys with an id that does not === the action.id and concat with action.toy
+      return [...state.filter(toy => toy.id !== action.toy.id), action.toy]
     case 'DELETE_TOY':
       return state.filter(toy => toy.id !== action.id)
     default: 
       return state
   }
 }
-
 
