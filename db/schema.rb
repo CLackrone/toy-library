61db704ade0259dd681eb2c8fce2d24b47fa521f
CLackrone <charisaanne@gmail.com>
2018-05-07 12:38:22 -0400

    added likes column to Toys

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180504160117) do
+ActiveRecord::Schema.define(version: 20180507163602) do
 
   create_table "toys", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20180504160117) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "description"
+    t.integer "likes"
   end
 
 end
